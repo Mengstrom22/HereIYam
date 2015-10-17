8d25379cc12a6675e6f4de110c423d9a6ae9ed13
Jef King <jef@agilebusinesscloud.com>
2015-10-17 15:16:39 -0700

    blah

--- HereIYam/www/scripts/main.js
+++ HereIYam/www/scripts/main.js
@@ -28,6 +28,7 @@ function clearWatch() {
         element.innerHTML = '';
     }
 }
+
 // onError Callback receives a PositionError object
 function onError(error) {
     alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
