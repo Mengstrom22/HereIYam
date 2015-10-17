79b59513ce83eff43eb18f559b9e9be7d72b2e79
Jef King <jef@agilebusinesscloud.com>
2015-10-17 14:26:25 -0700

    first run

--- HereIYam/www/scripts/index.js
+++ HereIYam/www/scripts/index.js
@@ -1,25 +1,100 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿function id(element) {
+    return document.getElementById(element);
+}
+ 
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function onDeviceReady() {
+    navigator.splashscreen.hide();
+    geolocationApp = new geolocationApp();
+    geolocationApp.run();
+    
+}
+ 
+function geolocationApp() {
+}
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+geolocationApp.prototype = {
+    _watchID: null,
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-    };
+    run: function () {
+        var that = this;
+        document.getElementById("watchButton").addEventListener("click", function () {
+            that._handleWatch.apply(that, arguments);
+        }, false);
+        document.getElementById("refreshButton").addEventListener("click", function () {
+            that._handleRefresh.apply(that, arguments);
+        }, false);
+    },
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    _handleRefresh: function () {
+        var options = {
+            enableHighAccuracy: true
+        },
+            that = this;
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+        that._setResults("Waiting for geolocation information...");
+
+        navigator.geolocation.getCurrentPosition(function () {
+            that._onSuccess.apply(that, arguments);
+        }, function () {
+            that._onError.apply(that, arguments);
+        }, options);
+    },
+
+    _handleWatch: function () {
+        var that = this,
+		// If watch is running, clear it now. Otherwise, start it.
+		button = document.getElementById("watchButton");
+
+        if (that._watchID != null) {
+            that._setResults();
+            navigator.geolocation.clearWatch(that._watchID);
+            that._watchID = null;
+
+            button.innerHTML = "Start Geolocation Watch";
+        }
+        else {
+            that._setResults("Waiting for geolocation information...");
+            // Update the watch every second.
+            var options = {
+                frequency: 1000,
+                enableHighAccuracy: true
+            };
+            that._watchID = navigator.geolocation.watchPosition(function () {
+                that._onSuccess.apply(that, arguments);
+            }, function () {
+                that._onError.apply(that, arguments);
+            }, options);
+            button.innerHTML = "Clear Geolocation Watch";
+
+        }
+    },
+
+    _onSuccess: function (position) {
+        // Successfully retrieved the geolocation information. Display it all.
+
+        this._setResults('Latitude: ' + position.coords.latitude + '<br />' +
+						 'Longitude: ' + position.coords.longitude + '<br />' +
+						 'Altitude: ' + position.coords.altitude + '<br />' +
+						 'Accuracy: ' + position.coords.accuracy + '<br />' +
+						 'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+						 'Heading: ' + position.coords.heading + '<br />' +
+						 'Speed: ' + position.coords.speed + '<br />' +
+						 'Timestamp: ' + new Date(position.timestamp).toLocaleTimeString().split(" ")[0] + '<br />');
+    },
+
+    _onError: function (error) {
+        this._setResults('code: ' + error.code + '<br/>' +
+						 'message: ' + error.message + '<br/>');
+    },
+
+    _setResults: function (value) {
+        if (!value) {
+            document.getElementById("results").innerHTML = "";
+        }
+        else {
+            document.getElementById("results").innerHTML = value;
+        }
+    },
+}
