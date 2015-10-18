6db1b19130d533b4815729e3393ec351a6cd6107
Jef King <jef@agilebusinesscloud.com>
2015-10-17 21:09:44 -0700

    working in

--- back/messaging/Controllers/GeoController.cs
+++ back/messaging/Controllers/GeoController.cs
@@ -6,18 +6,26 @@
     public class GeoController : ApiController
     {
         // POST api/geo
-        [HttpPost]
-        public IHttpActionResult Post(ClientLocation cl)
+        /// <summary>
+        /// Log current location, and share it
+        /// </summary>
+        /// <param name="location">Client Location</param>
+        /// <returns>none</returns>
+        /// <remarks>Multi-casts location to all listeners</remarks>
+        /// <response code="400">Bad request</response>
+        /// <response code="500">Internal Server Error</response>
+        [HttpPut]
+        public IHttpActionResult Put(ClientLocation location)
         {
-            if (null == cl)
+            if (null == location)
             {
                 return base.BadRequest("unknown lat/long");
             }
-            if (0 >= cl.latitude)
+            if (0 >= location.latitude)
             {
                 return base.BadRequest("unknown latitude");
             }
-            if (0 >= cl.longitude)
+            if (0 >= location.longitude)
             {
                 return base.BadRequest("unknown longitude");
             }
