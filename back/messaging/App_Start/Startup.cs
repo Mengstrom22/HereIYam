58d05eb58dfc0a8677f46682e0c40c1633bf5968
Jef King <jef@agilebusinesscloud.com>
2015-10-17 21:46:12 -0700

    more goodness

---
+++ back/messaging/App_Start/Startup.cs
@@ -0,0 +1,14 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(HereIYam.App_Start.Startup))]
+namespace HereIYam.App_Start
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
--- back/messaging/App_Start/WebApiConfig.cs
+++ back/messaging/App_Start/WebApiConfig.cs
@@ -6,8 +6,6 @@
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
