2974c892c1c3b906860f75f1bf8285920d307b6c
Jef King <jef@agilebusinesscloud.com>
2015-10-20 10:46:37 -0700

    make calls clearer

--- back/messaging/Core/GeoHub.cs
+++ back/messaging/Core/GeoHub.cs
@@ -6,7 +6,7 @@
     [HubName("geo")]
     public class GeoHub : Hub
     {
-        public void MultiCast(double lat, double lng)
+        public void MyLocation(double lat, double lng)
         {
             Clients.All.multicast(lat, lng);
         }
