6b9cb78773aa6ae48a8502841c3693de9b941b00
Jef King <jef@agilebusinesscloud.com>
2015-10-20 11:01:30 -0700

    more goodness

--- front/HereIYam/www/scripts/main.js
+++ front/HereIYam/www/scripts/main.js
@@ -18,12 +18,14 @@ function onSuccess(position) {
 
     var output = document.getElementById("out");
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
     var img = new Image();
     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
     output.appendChild(img);
+
+
+    var lattxt = document.getElementById("lattxt").value = latitude;
+    var longtxt = document.getElementById("lngtxt").value = longitude;
 }
 
 // onError Callback receives a PositionError object
